08906343e7a780a6575298d0b13c9264c782c167
jedus0r <94338687+jedus0r@users.noreply.github.com>
2021-11-15 06:31:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["YouTuber", "An Architect", "Blogger", "An Instructor", "Developer", "Freelancer"],
+        strings: ["An Pentester", "Bug Bounty Hunter"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["YouTuber", "Architect", "Blogger", "Instructor", "Developer", "Freelancer"],
+        strings: ["An Pentester", "Bug Bounty Hunter"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
