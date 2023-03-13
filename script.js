c126cd11e1e103f5240914b1ca35eba9ebc94ba0
jedus0r <94338687+jedus0r@users.noreply.github.com>
2023-03-13 17:51:09 +0100

    Update script.js

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Pentester", "Bug Bounty Hunter", "Offensive Security Certified Professional"],
+        strings: ["Pentester", "Security Researcher"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
